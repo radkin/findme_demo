7cfd7d7e33d787bc276ba5ecce2770502ed0095a
radkin <noeldamonmiller@gmail.com>
2020-06-25 09:09:15 -0700

    parse_search proper tests to go with our library

--- test/findme_parse_test.rb
+++ test/findme_parse_test.rb
@@ -6,14 +6,18 @@ require_relative '../lib/parse/parse_results'
 class UserProvidedInformationTest < Minitest::Test
 
   # should return a list of web links that nokogiri can parse
-  def test_return_web_links
+  def test_return_web_links_not_nil
     full_name_query = "jane+smith"
     query           = SearchClient.new(full_name_query)
     query_result    = query.search()
-    #puts (query_result.body)
     pr              = ParseResults.new(query_result.body)
     only_links       = pr.parse
-    puts only_links
+    # no empty values in our array
+    assert only_links.map{|link| !link.nil?}
+    # no google searches as we want only result links
+    assert only_links.map{|link| !link.match('google.com')}
+    # everything must start with https
+    assert only_links.map{|link| link.match(/^https:\/\//)}
   end
 
 end
