971204bf64bb4c4e6328947635f0ae64eb705424
radkin <noeldamonmiller@gmail.com>
2020-06-25 16:51:59 -0700

    threaded_search now testing direct links and search queries
    we're returning an object that has both search queries that google
    recommends and direct links for search results. This object (hash) has
    both and not we're prepped for using these recommended searches from
    google (hopefully other search engines work the same way) in a threaded,
    parallel way async to follow the links and see what we get from those.

--- lib/parse/parse_results.rb
+++ lib/parse/parse_results.rb
@@ -9,13 +9,19 @@ class ParseResults
   def parse
     all_links = Hash.new()
     direct_links = []
+    search_queries = []
     html_doc  = Nokogiri::HTML(@query_result)
     nodeset   = html_doc.xpath('//a')
     links = nodeset.map {|element| element["href"]}.compact
 
-    recommended_search_links = links.map {|all_links| all_links.split(/^\/search\?ie=UTF-8&q=/)[1]}
-    raw_query_links = links.map {|all_links| all_links.split(/^\/url\?q=/)[1] }
+    raw_search_queries = links.map {|all_links| all_links.split(/^\/search\?ie=UTF-8&q=/)[1]}
+    raw_search_queries.each do |search_query|
+      if !search_query.nil?
+        search_queries.push(search_query)
+      end
+    end
 
+    raw_query_links = links.map {|all_links| all_links.split(/^\/url\?q=/)[1] }
     raw_query_links.each do |link|
       # TIGHTLY COUPLED TO GOOGLE . FIX THIS
       if !link.nil? && !link.match('google.com')
@@ -26,8 +32,8 @@ class ParseResults
     end
 
     all_links = {
-      "direct"              => direct_links,
-      "recommended_search"  => recommended_search_links
+      "direct"          => direct_links,
+      "search_queries"  => search_queries
     }
     return all_links
   end
--- test/findme_parse_test.rb
+++ test/findme_parse_test.rb
@@ -5,21 +5,26 @@ require_relative '../lib/parse/parse_results'
 
 class ParseTest < Minitest::Test
 
-  # should return a list of web links that nokogiri can parse
-  def test_return_web_links_not_nil
-    full_name_query = "jane+smith"
-    query           = SearchClient.new(full_name_query)
-    query_result    = query.search()
-    pr              = ParseResults.new(query_result.body)
-    all_links       = pr.parse
+  full_name_query = "jane+smith"
+  query           = SearchClient.new(full_name_query)
+  query_result    = query.search()
+  pr              = ParseResults.new(query_result.body)
+  # class scope for thie re-used hash
+  @@all_links       = pr.parse
 
+  # should return a list of web links that nokogiri can parse
+  def test_return_direct_links
     # no empty values in our array
-    assert all_links['direct'].map{|link| !link.nil?}
+    assert @@all_links['direct'].map{|link| !link.nil?}
     # no google searches as we want only result links
-    assert all_links['direct'].map{|link| !link.match('google.com')}
+    assert @@all_links['direct'].map{|link| !link.match('google.com')}
     # everything must start with https
-    assert all_links['direct'].map{|link| link.match(/^https:\/\//)}
+    assert @@all_links['direct'].map{|link| link.match(/^https:\/\//)}
   end
 
+  def test_return_search_queries
+    # no empty values in our array
+    assert @@all_links['search_queries'].map{|link| !link.nil?}
+  end
 
 end
