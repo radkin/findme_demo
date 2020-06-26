c153217db9164b05adb73f4b973effbdae1a0ca2
radkin <noeldamonmiller@gmail.com>
2020-06-26 06:43:23 -0700

    linters_code_metrics simple bash script to run all tests
    since this is a standalone app and there's no ROR framework for rake
    helper.rb, etc. using bash to tie all testing together

---
+++ test/run_all_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "NOTE: this scipt is designed for you to run it from the root dir"
+echo "Example: cd findme_demo; test/run_all_tests.sh"
+
+echo "running unit tests"
+jruby test/findme_search_test.rb
+jruby test/findme_parse_test.rb
+jruby test/findme_threaded_test.rb
+
+echo "running rubocop standalone linter"
+bundle exec rubocop
