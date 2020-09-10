647d8c30d862342cef5ac7680cc2b6e0cc629ddf
radkin <noeldamonmiller@gmail.com>
2020-09-10 13:35:41 -0700

    travis split into stages
    more flexibility this way

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -5,17 +5,15 @@ echo "Example: cd findme_demo; test/run_all_tests.sh"
 
 echo "================== running functional tests =================="
 
-echo "---- search_test ----"
-jruby test/live_data/search_test.rb
-
-echo "---- search_parse_all_engines_test ----"
-jruby test/live_data/search_parse_all_engines_test.rb
-
 echo "---- producer role suite of tests ----"
-bash test/live_data/run_producer_role_tests.sh
+bash test/run_producer_role_tests.sh
 
-echo "---- threaded_test ----"
-jruby test/live_data/thread_search_engines_rest_test.rb
+echo "---- by library suite of tests ----"
+bash test/run_by_library_tests.sh
 
+echo "---- live data suite of tests ----"
+bash test/run_live_data_tests.sh
+
+# WHOLE THING
 echo "end to end test"
 jruby test/live_data/everything_test.rb
